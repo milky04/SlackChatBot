c987f50f030e953987245d12ae5f0eb82a425bf8
takumilky <76756065+takumilky@users.noreply.github.com>
2022-06-20 01:50:38 +0900

    Add files via upload

---
+++ SlackChatBot/ChatBot.py
@@ -0,0 +1,46 @@
+# Slackライブラリ
+from slack_sdk import WebClient
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+# 正規表現
+import re
+# rinna株式会社日本語版GPT-2事前学習モデル
+import RinnaJapaneseGPT2
+# rinnaを変数に格納
+rinna = RinnaJapaneseGPT2
+
+# トークン
+SLACK_BOT_TOKEN = "Bot User OAuth Tokenを記述"
+SLACK_APP_TOKEN = "Socket Mode のTokenを記述"
+
+# botを動かすチャンネルのID
+CHANNEL_ID = "botを動かすチャンネルIDを記述"
+# チャンネルIDを変数に格納
+channel=CHANNEL_ID
+
+# トークンを変数に格納
+token = WebClient(SLACK_BOT_TOKEN)
+
+# ログイン時にメッセージを送信する処理
+token.chat_postMessage(channel=channel, text="ログインしました。このチャンネルで話しかけてください。")
+
+# bot初期化
+app = App(token=SLACK_BOT_TOKEN)
+
+# 投稿されたメッセージに対してrinnaで生成された文章で返信
+@app.message(re.compile("(.*)"))
+def reply(message, context, say):
+    # 正規表現のマッチ結果がcontext.matchesに設定される
+    greeting = context['matches'][0]
+    # 投稿されたメッセージをrinnaに投げて生成された文章を受け取る
+    response = rinna.nlp(greeting)
+    # メッセージを投稿したユーザーを判定
+    user = message["user"]
+    # メンション付きメッセージを指定チャンネルに返す
+    text = f'<@{user}>{response}'
+    say(text=text, channel=channel)
+
+# bot起動
+if __name__ == "__main__":
+  print('start slackbot')
+  SocketModeHandler(app, SLACK_APP_TOKEN).start()
---
+++ SlackChatBot/RinnaJapaneseGPT2.py
@@ -0,0 +1,59 @@
+# ransformerライブラリを介してrinna株式会社日本語版GPT-2事前学習モデルを呼び出し
+from transformers import T5Tokenizer, AutoModelForCausalLM
+
+# rinna株式会社日本語版GPT-2事前学習モデル
+model_name = 'rinna/japanese-gpt2-medium'
+# tokenizerとmodelの生成
+# tokenizerで文字列をトークンにエンコード/デコード
+tokenizer = T5Tokenizer.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(model_name)
+
+# 受け取ったメッセージに対して文章を生成する処理
+# input_messageにチャットボットに返答させる文章を格納
+def nlp(input_message):
+    # tokenizerでエンコード
+    # 発言者(ユーザーとAI)を区別することで対話文と認識させる
+    input_ids = tokenizer.encode(
+        "私: " + input_message + "\nAI: ",
+        return_tensors="pt"
+    )
+    # generate関数で文章生成
+    output_sequences = model.generate(
+        # エンコードしたメッセージ
+        input_ids=input_ids,
+        # 文章生成文字数上限
+        max_length=50,
+        # 値が1に近づくほどクリエイティブな文章、0に近づくと論理的で正確な返答になる…らしい
+        temperature=0.9,
+        # top_kとtop_pの値は排他で、片方を>0に設定したら片方を0にする必要がある
+        # 確率の上位候補の絞り込み数を設定
+        top_k=0,
+        # 確率が一定以上の単語に絞る。下限となる確率を0～1の範囲で設定
+        top_p=0.9,
+        # 1に近づけると同じ文章の繰り返しを減少する効果
+        repetition_penalty=1.0,
+        # num_return_sequencesを複数回指定する場合Trueにする
+        do_sample=True,
+        # 一度の実行で生成する文章の数を指定
+        num_return_sequences=1)
+
+    # トークンの状態の生成文章が二次元リストで格納されているので二次元リスト0番目のリストを取得
+    output_sequence = output_sequences.tolist()[0]
+    # tokenizerでデコード
+    text = tokenizer.decode(
+        output_sequence, clean_up_tokenization_spaces=True)
+
+    # 最初に与えた入力文の長さを取得
+    input_ids_length = len(tokenizer.decode(input_ids[0], clean_up_tokenization_spaces=True))
+    # デコードされた文章から最初に与えた入力文を除去
+    total_text = (text[input_ids_length:])
+
+    # 生成文に'AI:'が含まれていた場合それ以降の文字列を除去
+    slice_potsition = total_text.find('AI:')
+    edited_text = total_text[:slice_potsition]
+    # 生成文に'私:'が含まれていた場合それ以降の文字列を除去
+    slice_potsition = edited_text.find('私:')
+    edited_text = edited_text[:slice_potsition]
+
+    # 生成された文章を返す
+    return edited_text
