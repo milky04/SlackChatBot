748571d77d011f92b3deaf0d0ee656e1e666119c
takumilky <76756065+takumilky@users.noreply.github.com>
2022-06-22 23:19:34 +0900

    Update RinnaJapaneseGPT1b.py

--- SlackChatBot/RinnaJapaneseGPT1b.py
+++ SlackChatBot/RinnaJapaneseGPT1b.py
@@ -2,7 +2,7 @@
 from transformers import T5Tokenizer, AutoModelForCausalLM
 # 正規表現
 import re
-# TensorとモデルのGPU/CPUを指定・切り替え
+# テンソルとモデルのGPU/CPUを指定・切り替え
 import torch
 
 # rinna株式会社日本語特化GPT言語モデル
@@ -14,26 +14,32 @@ model = AutoModelForCausalLM.from_pretrained(model_name)
 
 # GPUが使用可能な環境かどうかを判定
 if torch.cuda.is_available():
-    # モデルのGPU/CPUを切り替え(デフォルトはCPU)
-    # GPUを使うことで文章生成時間を短縮
+    # モデルのGPU/CPUを切り替え(デフォルトはCPU。サポートされているGPUはNVIDIA製のみ)
+    # GPUを使うことで文章生成時間を短縮(GPUは演算能力・並列処理能力に優れており、CPUよりも適しているため(ディープラーニングで行う行列演算と三次元グラフィックス処理時の行列演算が同じ))
+    # GPU(CPU)上にモデルのコピーを作成して上書き
     model = model.to("cuda")
-
+    
 # 受け取ったメッセージに対して文章を生成する処理
 # input_messageにチャットボットに返答させる文章を格納
 def nlp(input_message):
-    # tokenizerでエンコード
+    # 受け取ったメッセージをtokenizerでエンコード
     # 発言者(ユーザーとAI)を区別することで対話文と認識させる
     input_ids = tokenizer.encode(
         "私: " + input_message + "\nAI: ",
+        # スペシャルトークンの自動追加を無効(自分で追加するため)
         add_special_tokens=False,
+        # PyTorchのテンソル型で返す(Transformerモデルの入力でテンソル形式に変換する必要があるため)
+        # 機械学習におけるテンソルは多次元配列とほぼ同義(=行列。GPUとテンソルの計算の相性が良い)
         return_tensors="pt"
     )
     
-    # テンソルの勾配の計算を不可にしてメモリ消費削減
+    # テンソル計算時の勾配情報を持たないことでメモリ消費を抑える
     with torch.no_grad():
         # generate関数で文章生成
         output_sequences = model.generate(
-            # エンコードしたメッセージ
+            # テンソル型のエンコードしたメッセージ
+            # テンソルのGPU/CPUをモデルと同じデバイス(GPU/CPU)に切り替え(PyTorchではモデル学習時にモデルとデータを同じデバイスに置く必要がある)
+            # デバイス(GPU/CPU)上にエンコードしたメッセージのコピーを作成
             input_ids.to(model.device),
             # 文章生成文字数上限
             max_length=50,
@@ -47,30 +53,42 @@ def nlp(input_message):
             top_p=0.95,
             # 1に近づけると同じ文章の繰り返しを減少する効果
             repetition_penalty=1.0,
-            # num_return_sequencesを複数回指定する場合Trueにする
+            # サンプリングを有効化
             do_sample=True,
             # 一度の実行で生成する文章の数を指定
             num_return_sequences=1,
-            # 特殊な単語のIDを指定
+            # モデルの入力に必要な情報となるスペシャルトークンを指定
+            # PAD(Padding)は使用されていない部分を埋めるためのトークン
             pad_token_id=tokenizer.pad_token_id,
+            # BOS(Beggin Of Sentence)はシーケンス(文章)の始まりを表すトークン
             bos_token_id=tokenizer.bos_token_id,
+            # EOS(End Of Sentence)はシーケンス(文章)の終わりを表すトークン
             eos_token_id=tokenizer.eos_token_id,
+            # 生成が許可されていないトークンIDリストを設定(禁止用語等の設定で使用)
+            # UNK(Unknown)は未知の語彙を表すトークン
             bad_word_ids=[[tokenizer.unk_token_id]]
         )
-
-    # トークンの状態の生成文章が二次元リストで格納されているので二次元リスト0番目のリストを取得
+        
+    # トークンの状態の生成された文章が二次元リストで格納されているので二次元リスト0番目のリストを取得
     output_sequence = output_sequences.tolist()[0]
-    # tokenizerでデコード
+    # 生成された文章をtokenizerでデコード
     text = tokenizer.decode(
-        output_sequence, clean_up_tokenization_spaces=True)
+        output_sequence,
+        # 余分に生成されたスペースを削除
+        clean_up_tokenization_spaces=True
+        )
 
     # 最初に与えた入力文の長さを取得
-    input_ids_length = len(tokenizer.decode(input_ids[0], clean_up_tokenization_spaces=True))
+    input_ids_length = len(tokenizer.decode(
+        input_ids[0],
+        # 余分に生成されたスペースを削除
+        clean_up_tokenization_spaces=True
+        ))
     # デコードされた文章から最初に与えた入力文を除去
     total_text = (text[input_ids_length:])
     
     # 生成文にpatternにある文字列が含まれていた場合それ以降の文字列を除去。含まれていなかった場合はそのまま出力
-    pattern = r"AI:|私:|俺:|僕:|あなた:|<unk>|</s>"
+    pattern = r"AI:|私:|俺:|僕:|あなた:|<unk>|</s>|[UNK]"
     if re.search(pattern, total_text) != None:
         # 文章中のpatternにある文字列の位置を全て抽出
         match_position = [match.span() for match in re.finditer(pattern, total_text)]
@@ -83,3 +101,4 @@ def nlp(input_message):
     else:
         # 生成された文章を返す
         return total_text
+    
